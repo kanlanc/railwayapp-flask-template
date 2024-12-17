42c4505868c013d3677f1ac35b8904c6fb4499b5
Vikranth Kanumuru <20688709+kanlanc@users.noreply.github.com>
2024-12-17 13:51:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if True:
+        data = request.get_json()
+        return jsonify({
+            "message": "Webhook received",
+            "data": data
+        }), 200
+    return jsonify({"error": "Request must be JSON"}), 400
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
